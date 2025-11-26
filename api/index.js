a82143c7d53d7a64cdc3628dd8dde32131e76022
muhazziz <muhammadazizsyahp@gmail.com>
2025-11-26 10:49:24 +0700

    fix: use emit request for hapi serverless adapter

--- api/index.js
+++ api/index.js
@@ -3,5 +3,5 @@ const init = require("../src/server"); // Pastikan path ini benar
 module.exports = async (req, res) => {
   const server = await init(); // Memanggil fungsi init yang diexport tadi
   await server.initialize();
-  return server.listener(req, res);
+  return server.listener.emit("request", req, res);
 };
