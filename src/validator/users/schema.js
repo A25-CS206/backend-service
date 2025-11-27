b31ad42a7cc74138910ed1ae4deba0f64c20da63
muhazziz <muhammadazizsyahp@gmail.com>
2025-11-27 16:40:20 +0700

    fix: ensure validator schema allows phone and city

--- src/validator/users/schema.js
+++ src/validator/users/schema.js
@@ -3,7 +3,10 @@ const Joi = require("joi");
 const UsersPayloadSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(), // Aturan: Password min 6 huruf
+  password: Joi.string().min(6).required(),
+  phone: Joi.string().required(),
+  city: Joi.string().required(),
+  imagePath: Joi.string().allow(null, ""),
 });
 
 module.exports = { UsersPayloadSchema };
